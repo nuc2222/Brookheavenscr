1b2e80786e2e01127c9d4f508ce525ea82f1aad0
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-23 17:37:25 +0300

    Update ScriptMain.lua

--- ScriptMain.lua
+++ ScriptMain.lua
@@ -1,1 +1,3 @@
 game:GetService("ReplicatedStorage").JK.TR["1Too1l"]:InvokeServer("PickingTools",giveitem)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = setspeed
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = setjumppower
