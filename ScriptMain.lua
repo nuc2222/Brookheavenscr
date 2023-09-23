3e02b6b4b2bddacd146e21f1dd835e335c96cb2a
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-23 20:39:15 +0300

    Update ScriptMain.lua

--- ScriptMain.lua
+++ ScriptMain.lua
@@ -1,10 +1,3 @@
 game:GetService("ReplicatedStorage").JK.TR["1Too1l"]:InvokeServer("PickingTools",giveitem)
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = setspeed
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = setjumppower
-if cmd == "help" do
-    print("[cmd] help: lists all cmds")
-    print("[var] giveitem: gives an item")
-    print("[var] setspeed: sets an speed")
-    print("[var] setjumppower: sets an jump power")
-    print("[cmd] infy: starts infinite yield")
-end
