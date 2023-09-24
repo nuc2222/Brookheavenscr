ed95e1663b32ac366d479274e58c7d225afa7290
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-24 13:41:16 +0300

    Create Scripthublib.lua

---
+++ Scripthublib.lua
@@ -0,0 +1,6 @@
+scripthub№ = {
+  'loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Brookheavenscr/main/ScriptMain.lua"))()',
+  'loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()',
+}
+function loadscript(number)
+  load(test[number] .. "")()
