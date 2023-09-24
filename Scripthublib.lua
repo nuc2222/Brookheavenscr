e1b7d9a1a433a82748fdbb5576da0bfd19e2d11f
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-24 21:27:19 +0300

    Update Scripthublib.lua

--- Scripthublib.lua
+++ Scripthublib.lua
@@ -2,6 +2,7 @@ print("loaded script hub!!!")
 scripthub№ = {
   'loadstring(game:HttpGet("https://raw.githubusercontent.com/nuc2222/Brookheavenscr/main/ScriptMain.lua"))()',
   'loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()',
+  'loadstring(game:HttpGet(("https://raw.githubusercontent.com/JustAP1ayer/BABFTPLAYERHUB/main/BABFTV2.lua"),true))()',
 }
 function loadscript(number)
   load(scripthub№[number] .. "")()
