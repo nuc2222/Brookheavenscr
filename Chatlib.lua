553db557700bbc8481f0b81ccea9b5458bca305b
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-24 19:58:59 +0300

    Update Chatlib.lua

--- Chatlib.lua
+++ Chatlib.lua
@@ -1,1 +1,8 @@
-
+print("loaded libary: chat")
+chatlogs = ""
+function firemessage(input)
+  game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(" el pepe","All")
+end
+function sendmessage(input)
+  
+end
