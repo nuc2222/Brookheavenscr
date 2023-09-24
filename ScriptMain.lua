0ebdd9538d8dc9ef235818b81c4b0a1ad0c533ea
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-24 13:06:34 +0300

    Update ScriptMain.lua

--- ScriptMain.lua
+++ ScriptMain.lua
@@ -1,6 +1,29 @@
+ldl = {}
+logs = {}
+ldll = {"movement","game","serverside","gui","chat","scripthub","sound","https","json","basic","","","","","","",""}
+function addlog(input)
+  
+function inlist(item, list)
+    for _, value in ipairs(list) do
+        if value == item then
+            return true
+        end
+    end
+    return false
+end
 function giveitem(input)
   game:GetService("ReplicatedStorage").JK.TR["1Too1l"]:InvokeServer("PickingTools",input)
-function setspeed()
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = setspeed
-function setjumppower(setjumppower)
-  game.Players.LocalPlayer.Character.Humanoid.JumpPower = setjumppower
+end
+function setspeed(input)
+  if inlist("movement", ldl) then
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = input
+  end
+end
+function setjumppower(input)
+  if inlist("movement", ldl) then
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = input
+  end
+end
+function loadlib(input)
+  table.insert(ldl, input)
+end
