586addc1ec80913b8df017fe9785e7cfc126974c
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-24 21:24:19 +0300

    Update Chatlib.lua

--- Chatlib.lua
+++ Chatlib.lua
@@ -1,8 +1,31 @@
 print("loaded libary: chat")
-chatlogs = ""
-function firemessage(input)
-  game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(" el pepe","All")
+chatlogNames = {}
+chatlogMessages = {}
+shouldLogChat = false
+function sendemessage(input)
+  game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(input,"All")
 end
-function sendmessage(input)
-  
+function onPlayerChat(player, message)
+    if shouldLogChat then
+        -- Add the message owner's name to the chatlogNames table
+        table.insert(chatlogNames, player.Name)
+        
+        -- Add the message content to the chatlogMessages table
+        table.insert(chatlogMessages, message)
+        
+        -- You can also print the message to the console if needed
+        print(player.Name .. " said: " .. message)
+    end
 end
+
+-- Function to toggle chat logging
+function logchat(enableLogging)
+    shouldLogChat = enableLogging
+end
+
+-- Connect the function to the PlayerChatted event
+game.Players.PlayerAdded:Connect(function(player)
+    player.Chatted:Connect(function(message)
+        onPlayerChat(player, message)
+    end)
+end)
