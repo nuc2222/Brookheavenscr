e2ff3988abdb1ab85e0e572b42349e65add909ad
nuc2222 <122896690+nuc2222@users.noreply.github.com>
2023-09-24 13:11:45 +0300

    Create Movementlib.lua

---
+++ Movementlib.lua
@@ -0,0 +1,10 @@
+function setspeed(input)
+  if inlist("movement", ldl) then
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = input
+  end
+end
+function setjumppower(input)
+  if inlist("movement", ldl) then
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = input
+  end
+end
